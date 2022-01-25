caab00860c4337a4af73899ce1009e835eb8809a
wwwaw <aold88@gmail.com>
2022-01-25 23:27:17 +0300

    fighting with grid & flexbox. losing...

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 let blob;
 
 async function getPhoto() {
-    let img = document.getElementsByClassName("randomImage");
-    //img.src = "https://source.unsplash.com/random?sig=" + Math.random();
+  let img = document.querySelector('.randomImage');
+  //img.src = "https://source.unsplash.com/random?sig=" + Math.random();
 
-    let response = await fetch('https://source.unsplash.com/random');
-    blob = await response.blob();
-    img.src = URL.createObjectURL(blob);
-    document.getElementsByClassName("hidden").style.visibility = "visible";
-    document.getElementsByClassName("ImageButton").textContent = "Get another random image";
-  }
+  let response = await fetch('https://source.unsplash.com/random');
+  blob = await response.blob();
+  img.src = URL.createObjectURL(blob);
+  document.querySelector('.hidden').style.visibility = 'visible';
+  //document.getElementsByClassName("hidden").style.visibility = "visible";
+  document.querySelector('.button').textContent = "Get another random image";
+}
 
-  function saveBlobAsFile(blob, fileName) {
-    var reader = new FileReader();
+function saveBlobAsFile(blob, fileName) {
+  var reader = new FileReader();
 
-    reader.onloadend = () => {
-        var base64 = reader.result;
-        var link = document.createElement("a");
+  reader.onloadend = () => {
+    var base64 = reader.result;
+    var link = document.createElement("a");
 
-        document.body.appendChild(link); // for Firefox
+    document.body.appendChild(link); // for Firefox
 
-        link.setAttribute("href", base64);
-        link.setAttribute("download", fileName);
-        link.click();
-    };
+    link.setAttribute("href", base64);
+    link.setAttribute("download", fileName);
+    link.click();
+  };
 
-    reader.readAsDataURL(blob);
+  reader.readAsDataURL(blob);
 }
