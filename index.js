e0b8f89e6f09be4d5b5b23750d78e3b10d6c60c4
abarakkadabra <konyahin_al@mail.ru>
2022-01-31 17:30:19 +0300

    //

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ let blob;
 
 async function getPhoto() {
   let img = document.querySelector('.randomImage');
-  //img.src = "https://source.unsplash.com/random?sig=" + Math.random();
 
   let response = await fetch('https://source.unsplash.com/random');
   blob = await response.blob();
   img.src = URL.createObjectURL(blob);
-  document.querySelector('.hidden').style.visibility = 'visible';
+  //document.querySelector('.hidden').style.visibility = 'visible';
   //document.getElementsByClassName("hidden").style.visibility = "visible";
   document.querySelector('.button').textContent = "Get another random image";
+  document.querySelector('.button').style.cssText = "top: 0; left: auto";
 }
 
 function saveBlobAsFile(blob, fileName) {
@@ -28,3 +28,5 @@ function saveBlobAsFile(blob, fileName) {
 
   reader.readAsDataURL(blob);
 }
+
+//<button class="button hidden" type="button" onclick="saveBlobAsFile(blob)">Save this fancy image</button>
