ed41b6b122c4e76ed67df3cce4f2012a966a44fe
abarakkadabra <konyahin_al@mail.ru>
2022-02-01 17:59:13 +0300

    //

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ async function getPhoto() {
   img.src = URL.createObjectURL(blob);
   //document.querySelector('.hidden').style.visibility = 'visible';
   //document.getElementsByClassName("hidden").style.visibility = "visible";
-  document.querySelector('.button').textContent = "Get another random image";
-  document.querySelector('.static').style.cssText = "position: float; top:0; right:0;";
-  document.getElementByClass('imgWrapper').appendChild(document.getElementByClass('button'))
+  document.querySelector('#button__getImage').textContent = "Get another random image";
+  
+  document.querySelector('#imgWrapper').appendChild(document.querySelector('#button__getImage'))
+  document.querySelector('#button__getImage').style.position = "float";;
+  //document.elm.style.position = "float";
+
 }
 
 function saveBlobAsFile(blob, fileName) {
